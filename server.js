2089cdd32c087c175f9dd79aef2928796f453c52
lucashss88 <lucashss88@gmail.com>
2024-09-06 09:03:16 -0300

    Adding cors

--- server.js
+++ server.js
@@ -8,7 +8,11 @@ const workoutRoutes = require('./routes/workoutRoutes');
 
 const app = express();
 
-app.use(cors());
+app.use(cors(
+  {
+    origin: '*'
+  }
+));
 
 app.use(bodyParser.json());
 
