aa86ef4221552fbe80713a4322b33551804fa932
lucashss88 <lucashss88@gmail.com>
2024-09-06 20:43:09 -0300

    fixing .env

--- config/database.js
+++ config/database.js
@@ -1,20 +1,21 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-const dbName = process.env.NODE_ENV === 'production' ? process.env.PGDATABASE : process.env.DB_NAME;
-const dbUser = process.env.NODE_ENV === 'production' ? process.env.PGUSER : process.env.DB_USER;
-const dbPassword = process.env.NODE_ENV === 'production' ? process.env.PGPASSWORD : process.env.DB_PASSWORD;
-const dbHost = process.env.NODE_ENV === 'production' ? process.env.PGHOST : process.env.DB_HOST;
-const dbPort = process.env.NODE_ENV === 'production' ? process.env.PGPORT : process.env.DB_PORT;
+// const dbName = process.env.NODE_ENV === 'production' ? process.env.PGDATABASE : process.env.DB_NAME;
+// const dbUser = process.env.NODE_ENV === 'production' ? process.env.PGUSER : process.env.DB_USER;
+// const dbPassword = process.env.NODE_ENV === 'production' ? process.env.PGPASSWORD : process.env.DB_PASSWORD;
+// const dbHost = process.env.NODE_ENV === 'production' ? process.env.PGHOST : process.env.DB_HOST;
+// const dbPort = process.env.NODE_ENV === 'production' ? process.env.PGPORT : process.env.DB_PORT;
 
-const sequelize = new Sequelize(dbName, dbUser, dbPassword, {
-  host: dbHost,
-  port: dbPort,
-  dialect: 'postgres', 
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  // host: dbHost,
+  // port: dbPort,
+  dialect: 'postgres',
+  protocol: 'postgres',
   dialectOptions: {
     ssl: {
-      require: true, // requer SSL
-      rejectUnauthorized: false // para ignorar problemas de certificado autoassinado
+      require: true, 
+      rejectUnauthorized: false 
     }
   },
   logging: false,
