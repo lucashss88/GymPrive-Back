0686fbab66aa7d1ceb2f238e4b31cf8807278015
lucashss88 <lucashss88@gmail.com>
2025-03-27 14:36:23 -0300

    new version 2025

--- config/database.js
+++ config/database.js
@@ -7,19 +7,24 @@ require('dotenv').config();
 // const dbHost = process.env.NODE_ENV === 'production' ? process.env.PGHOST : process.env.DB_HOST;
 // const dbPort = process.env.NODE_ENV === 'production' ? process.env.PGPORT : process.env.DB_PORT;
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
-  // host: dbHost,
-  // port: dbPort,
-  dialect: 'postgres',
-  protocol: 'postgres',
-  dialectOptions: {
-    ssl: {
-      require: true, 
-      rejectUnauthorized: false 
-    }
-  },
-  logging: false,
-});
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD, 
+  {
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT,
+    port: process.env.DB_PORT,
+    protocol: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true, 
+        rejectUnauthorized: false 
+      }
+    },
+    logging: false,
+  }
+);
 
 sequelize.authenticate()
   .then(() => console.log('Postgres connected...'))
